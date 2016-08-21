b3559e152c36cb3736f0ad21e46702df1c5d1014
Adrien Gibrat <adrien.gibrat@gmail.com>
2016-08-21 16:36:20 +0200

    remove test file

--- src/untitled.js
+++
@@ -1,9 +0,0 @@
-script = document.createElement('script')
-script.type = 'text/javascript'
-script.src = 'https://rawgit.com/jlongster/transducers.js/master/dist/transducers.js'
-document.body.appendChild(script)
-
-script = document.createElement('script')
-script.type = 'text/javascript'
-script.src = 'https://cdn.jsdelivr.net/g/lodash@4.12.0,lazy.js@0.4.2'
-document.body.appendChild(script)
