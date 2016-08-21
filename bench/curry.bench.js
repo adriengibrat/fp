88fb592013beb32862e4486325f6ecbe98714c0d
Adrien Gibrat <adrien.gibrat@gmail.com>
2016-08-21 20:15:51 +0200

    update curry bench

--- bench/curry.bench.js
+++ bench/curry.bench.js
@@ -3,34 +3,39 @@
 const benchmark = require('benchmark').Suite
 
 const fp = require('../dist/fp')
-const lodash = {curry: require('lodash/curry')}
+const lodash = { curry: require('lodash/curry') }
 const ramda = require('ramda')
 ramda.curry.placeholder = ramda.__
 
+const bench = (test, valid) => new benchmark(test.name)
+	.add('fp.curry', test(fp.curry))
+	.add('fp.curry.debug', test(fp.curry.debug))
+	.add('lodash.curry', test(lodash.curry))
+	.add('ramda.curry', test(ramda.curry))
+	.on('start', event => console.log('Bench %s', event.currentTarget.name))
+	.on('cycle', event => console.log('%s %s', valid(event.target.fn) ? '✓' : '✗', event.target))
+	.on('complete', event => console.log('-> Fastest is %s', event.currentTarget.filter('fastest').map('name')))
+	.run()
+
 function fn (a, b) {
-  return [].slice.call(arguments)
+	return [].slice.call(arguments)
 }
 
-const noCurry = () => {
-	const a = 1
-	const b = 2
-	return fn(a, b)
-}
-const manual = () => ((a) => (b) => fn(a, b))(1)(2)
+// currying process speed
+bench(function currying (curry) {
+	return () => curry(fn)
+}, fn => fn().length === 2)
 
-const bench = (curry) => {
-	const curried = curry(fn) // bench curried function, not currying process
+const valid = fn => JSON.stringify(fn()) === '[1,2]'
+
+// curried function speed
+bench(function basics (curry) {
+	const curried = curry(fn)
+	return () => curried(1)(2)
+}, valid)
+
+// curried function speed using placeholder
+bench(function placeholder (curry) {
+	const curried = curry(fn) 
 	return () => curried(curry.placeholder, 2)(1)
-}
-const valid = (fn) => JSON.stringify(fn()) === '[1,2]' ? '✓' : '✗'
-
-new benchmark()
-	// .add('no curry', noCurry)
-	// .add('manual curry', manual)
-	.add('fp.curry', bench(fp.curry))
-	.add('fp.curry.debug', bench(fp.curry.debug))
-	.add('lodash.curry', bench(lodash.curry))
-	.add('ramda.curry', bench(ramda.curry))
-	.on('cycle', (event) => console.log(valid(event.target.fn), String(event.target)))
-	.on('complete', function () { console.log('\nFastest is ' + this.filter('fastest').map('name')) })
-	.run({ async: true })
+}, valid)
